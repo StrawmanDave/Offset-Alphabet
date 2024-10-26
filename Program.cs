8a28ed620fab927a37a8f1a1abddb0e650b5f7c3
StrawmanDave <davidgb.strawman@gmail.com>
2024-10-26 15:33:11 -0700

    offset any string

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// David 10/26/24 Offset Alphabet
+
+Console.Clear();
+int i = 0;
+int Offset = ReadNumber();
+string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+OffSetString(Offset,alphabet);
+
+void OffSetString(int AmountToOffset, string WhatToOffset)
+{
+    while(amountOffset>i)
+    {
+        WhatToOffset = WhatToOffset.Substring(1) + WhatToOffset[0];
+        i++;
+    }
+    Console.WriteLine(WhatToOffset);
+}
+int ReadNumber()
+{
+    while(true)
+    {
+        Console.Write("Please Enter a number ");
+        string num = Console.ReadLine();
+        if(!"123456789".Contains(num)) // Checks if the user put in a number
+        {
+            Console.WriteLine("That was not a number try again");
+        }else
+        {
+            int number = Convert.ToInt32(num);
+            return number;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Offset-Alphabet
+This program is meant to practice using Methods/functions in C# it takes a number from the user then offsets the alphabet that many places.
